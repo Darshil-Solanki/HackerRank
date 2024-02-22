3c80b9ec09dd90923b23862a511287cd2c0ebf00
Darshil Solanki <119782708+Darshil-Solanki@users.noreply.github.com>
2024-02-22 23:14:00 +0530

    Problem: Breaking minimum and maximum records

---
+++ breakingRecords.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'breakingRecords' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY scores as parameter.
+#
+
+def breakingRecords(scores):
+    # Write your code here
+    mn=mx=0
+    minValue=maxValue=scores[0]
+    for i in scores:
+        if(i<minValue):
+            minValue=i
+            mn+=1
+        elif(i>maxValue):
+            maxValue=i
+            mx+=1
+        else:
+            pass
+    return [mx,mn]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    scores = list(map(int, input().rstrip().split()))
+
+    result = breakingRecords(scores)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
