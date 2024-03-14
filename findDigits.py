209f8e5c1f68ef22ada2c31256465d7c3902a1c3
Darshil Solanki <119782708+Darshil-Solanki@users.noreply.github.com>
2024-03-14 17:39:51 +0530

    Problem: Find Digits and Extra Long Factorials

---
+++ findDigits.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'findDigits' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def findDigits(n):
+    # Write your code here
+    temp,c=n,0
+    while n>0:
+        while n%10==0:
+            n//=10  
+        if(temp%(n%10)==0):
+            c+=1
+        n//=10
+    return c
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = findDigits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
