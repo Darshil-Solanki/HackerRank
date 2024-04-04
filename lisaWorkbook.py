3c5ec7fc0668fba14beb53a45bfb46b70b2746cf
Darshil Solanki <119782708+Darshil-Solanki@users.noreply.github.com>
2024-04-04 18:17:45 +0530

    Problem: Lisa Workbook

---
+++ lisaWorkbook.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'workbook' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER k
+#  3. INTEGER_ARRAY arr
+#
+
+def workbook(n, k, arr):
+    # Write your code here
+    cp = 0
+    spCount = 0
+    for i in arr:
+        for j in range(1,i+1,k):
+            cp+=1
+            if j<=cp<=(j+k-1 if (j+k-1)<i else i):
+                spCount += 1
+    return spCount
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = workbook(n, k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
