2eaad811b0a08ed4cc08bdbdb8716a26f9d1ff5f
Darshil Solanki <119782708+Darshil-Solanki@users.noreply.github.com>
2024-04-23 09:35:39 +0530

    Problem: Caser-Cipher

---
+++ caserCipher1.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'caesarCipher' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. INTEGER k
+#
+
+def caesarCipher(s, k):
+    # Write your code here
+    result = ''
+    for char in s:
+        if char.isalpha():
+            base = 65 if char.isupper() else 97
+            result += chr((ord(char) - base + k) % 26 + base)
+        else:
+            result += char
+    return result
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    s = input()
+
+    k = int(input().strip())
+
+    result = caesarCipher(s, k)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
