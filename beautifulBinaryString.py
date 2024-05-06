56b164a424a2d8f34fb56fea5fbec41a75b7ad05
Darshil Solanki <119782708+Darshil-Solanki@users.noreply.github.com>
2024-05-06 09:48:17 +0530

    Problem: Beautiful Binary String

---
+++ beautifulBinaryString.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'beautifulBinaryString' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING b as parameter.
+#
+
+def beautifulBinaryString(b):
+    # Write your code here
+    if len(b)<3:
+        return 0
+    p = r'010'
+    return len(re.findall(p, b))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    b = input()
+
+    result = beautifulBinaryString(b)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
