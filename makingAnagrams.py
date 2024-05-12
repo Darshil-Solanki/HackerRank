bc23baeb4a33d3d11eb7b97814155b2847d32945
Darshil Solanki <119782708+Darshil-Solanki@users.noreply.github.com>
2024-05-12 10:53:38 +0530

    Problem: Making Anagram

---
+++ makingAnagrams.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'makingAnagrams' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. STRING s1
+#  2. STRING s2
+#
+
+def makingAnagrams(s1, s2):
+    # Write your code here
+    unCh = set(s1).union(set(s2))
+    count = 0
+    for ch in unCh:
+        count += abs(s1.count(ch)-s2.count(ch))
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s1 = input()
+
+    s2 = input()
+
+    result = makingAnagrams(s1, s2)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
