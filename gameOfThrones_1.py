0d7139a63c92d74303ffa9048b4a2570a704bb82
Darshil Solanki <119782708+Darshil-Solanki@users.noreply.github.com>
2024-05-13 13:22:49 +0530

    Problem: Game Of Thrones 1

---
+++ gameOfThrones_1.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gameOfThrones' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def gameOfThrones(s):
+    # Write your code here
+    unChs = set(s)
+    oddCount = 0
+    for ch in unChs:
+        if s.count(ch)%2!=0:
+            oddCount+=1
+            if oddCount>1:
+                return "NO"
+    return "YES"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = gameOfThrones(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
