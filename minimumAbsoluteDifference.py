98aeb7923fcd742578f9ab2fb4beb3b2d0685d42
Darshil Solanki <119782708+Darshil-Solanki@users.noreply.github.com>
2024-05-16 10:05:13 +0530

    Problem: Minimum Absolute Difference

---
+++ minimumAbsoluteDifference.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'minimumAbsoluteDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def minimumAbsoluteDifference(arr):
+    # Write your code here
+    arr.sort()
+    minDiff = float("inf")
+    for i in range(1, len(arr)):
+        minDiff = abs(arr[i]-arr[i-1]) if minDiff > abs(arr[i]-arr[i-1]) else minDiff
+    return minDiff
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = minimumAbsoluteDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
