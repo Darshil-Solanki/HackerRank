b8018165304ff5ce70fe2b5bbd19787acec9ba31
Darshil Solanki <119782708+Darshil-Solanki@users.noreply.github.com>
2024-05-25 17:09:25 +0530

    Problem: Jim And The Orders

---
+++ jimAndTheOrders.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'jimOrders' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts 2D_INTEGER_ARRAY orders as parameter.
+#
+
+def jimOrders(orders):
+    # Write your code here
+    prepTime = [(order[0]+order[1],idx+1) for idx,order in enumerate(orders)]
+    prepTime.sort(key= lambda x: x[0])
+    return [time[1] for time in prepTime]
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    orders = []
+
+    for _ in range(n):
+        orders.append(list(map(int, input().rstrip().split())))
+
+    result = jimOrders(orders)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
