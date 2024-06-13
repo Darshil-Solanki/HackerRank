cd7ba61f78a507b82f75125520a6c5bdc2a3a2a0
Darshil Solanki <119782708+Darshil-Solanki@users.noreply.github.com>
2024-06-13 18:54:06 +0530

    Problem: The Great XOR

---
+++ theGreatXOR.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'theGreatXor' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER x as parameter.
+#
+
+def theGreatXor(x):
+    # Write your code here
+    return int("1"*(len(bin(x))-2), 2)-x
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        x = int(input().strip())
+
+        result = theGreatXor(x)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
+
