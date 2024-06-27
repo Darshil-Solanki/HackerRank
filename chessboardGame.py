9e1ca4cb7b3150e44732847b5036860fc505dff6
Darshil Solanki <119782708+Darshil-Solanki@users.noreply.github.com>
2024-06-27 11:04:39 +0530

    Problem: ChessBoard Game

---
+++ chessboardGame.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'chessboardGame' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER x
+#  2. INTEGER y
+#
+
+def chessboardGame(x, y):
+    # Write your code here
+    x%=4; y%=4
+    return "First" if y==0 or y==3 or x==0 or x==3 else "Second"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        x = int(first_multiple_input[0])
+
+        y = int(first_multiple_input[1])
+
+        result = chessboardGame(x, y)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
+
